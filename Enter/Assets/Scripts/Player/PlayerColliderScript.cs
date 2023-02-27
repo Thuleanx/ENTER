5b00ae9a922b6cdf760cddadc22d708d8ca1b6c8
Zack Cheng <zack_cheng@brown.edu>
2023-02-26 19:53:43 -0500

    Change ground check to linecast

--- Enter/Assets/Scripts/Player/PlayerColliderScript.cs
+++ Enter/Assets/Scripts/Player/PlayerColliderScript.cs
@@ -7,18 +7,19 @@ namespace Enter
   public class PlayerColliderScript : MonoBehaviour
   {
     [Header("Layers")]
-    [SerializeField] private LayerMask _envLayer;
+    [SerializeField] private LayerMask _solidLayers;
+    [SerializeField] private LayerMask _rcBoxLayer;
 
     [Header("Collider Bounds")]
     [SerializeField] private float _halfH = 0.25f;
     [SerializeField] private float _halfW  = 0.25f;
 
-    [Header("Ground Circle Collider")]
-    [SerializeField] private float _collisionRadius = 0.1f;
+    [Header("Ground Linecast Position Tweaks")]
+    [SerializeField] private float _offset = 0.02f;
 
-    [Header("Raycast Position Tweaks")]
+    [Header("Overhead Raycast Position Tweaks")]
     [SerializeField] private float _rayDistance = 0.5f;
-    [SerializeField] private float _outerFrac = 1.01f;
+    [SerializeField] private float _outerFrac = 1.02f;
     [SerializeField] private float _innerFrac = 0.25f;
 
     private Vector2 _lineOffset => new Vector2(0, _rayDistance);
@@ -41,12 +42,15 @@ namespace Enter
 
     void FixedUpdate()
     {
-      OnGround = Physics2D.OverlapCircle(getOffsetPosition(0, -_halfH), _collisionRadius, _envLayer);
+      OnGround = Physics2D.Linecast(
+        getOffsetPosition(-_halfW, -_halfH - _offset),
+        getOffsetPosition(+_halfW, -_halfH - _offset),
+        _solidLayers);
 
-      TopLeftmost  = Physics2D.Raycast(getOffsetPosition(-_halfW * _outerFrac, _halfH), Vector2.up, _rayDistance, _envLayer).collider != null;
-      TopLeft      = Physics2D.Raycast(getOffsetPosition(-_halfW * _innerFrac, _halfH), Vector2.up, _rayDistance, _envLayer).collider != null;
-      TopRight     = Physics2D.Raycast(getOffsetPosition(+_halfW * _innerFrac, _halfH), Vector2.up, _rayDistance, _envLayer).collider != null;
-      TopRightmost = Physics2D.Raycast(getOffsetPosition(+_halfW * _outerFrac, _halfH), Vector2.up, _rayDistance, _envLayer).collider != null;
+      TopLeftmost  = Physics2D.Raycast(getOffsetPosition(-_halfW * _outerFrac, _halfH), Vector2.up, _rayDistance, _solidLayers).collider != null;
+      TopLeft      = Physics2D.Raycast(getOffsetPosition(-_halfW * _innerFrac, _halfH), Vector2.up, _rayDistance, _solidLayers).collider != null;
+      TopRight     = Physics2D.Raycast(getOffsetPosition(+_halfW * _innerFrac, _halfH), Vector2.up, _rayDistance, _solidLayers).collider != null;
+      TopRightmost = Physics2D.Raycast(getOffsetPosition(+_halfW * _outerFrac, _halfH), Vector2.up, _rayDistance, _solidLayers).collider != null;
 
       if (TopLeftmost ^ TopRightmost) 
       {
@@ -58,7 +62,9 @@ namespace Enter
     {
       Gizmos.color = Color.red;
 
-      Gizmos.DrawWireSphere(getOffsetPosition(0, -_halfH), _collisionRadius);
+      Gizmos.DrawLine(
+        getOffsetPosition(-_halfW, -_halfH - _offset),
+        getOffsetPosition(+_halfW, -_halfH - _offset));
 
       Gizmos.DrawLine(getOffsetPosition(-_halfW * _outerFrac, _halfH), getOffsetPosition(-_halfW * _outerFrac, _halfH) + _lineOffset);
       Gizmos.DrawLine(getOffsetPosition(-_halfW * _innerFrac, _halfH), getOffsetPosition(-_halfW * _innerFrac, _halfH) + _lineOffset);
