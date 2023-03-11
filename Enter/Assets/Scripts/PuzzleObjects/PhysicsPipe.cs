39cc4728a767c944e5095b7626aef3777f676c12
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-03-11 16:06:17 -0500

    physics pipe that spawns physics box

--- Enter/Assets/Scripts/PuzzleObjects/PhysicsPipe.cs
+++ Enter/Assets/Scripts/PuzzleObjects/PhysicsPipe.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
+using NaughtyAttributes;
 
 namespace Enter {
 	public class PhysicsPipe : MonoBehaviour {
 		[SerializeField] float blocksPerMinute;
+		[ShowAssetPreview, SerializeField] GameObject physicsObjectPrefab;
+		float timeSinceLastSpawn = -1;
 
 		// sadguh no object pool
 		public void CreatePhysicsObject() {
+			Instantiate(physicsObjectPrefab, transform.position, Quaternion.identity);
 		}
 
 		void Update() {
-			
+			if (timeSinceLastSpawn + 60f / blocksPerMinute < Time.time) {
+				CreatePhysicsObject();
+				timeSinceLastSpawn = Time.time;
+			}
 		}
 	}
 }
