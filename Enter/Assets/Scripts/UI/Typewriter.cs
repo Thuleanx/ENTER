8a24adb6cc81b56f0e915f138de2ccfe0603198c
Thuleanx <59397202+Thuleanx@users.noreply.github.com>
2023-04-15 22:27:35 -0400

    type writer effect

--- Enter/Assets/Scripts/UI/Typewriter.cs
+++ Enter/Assets/Scripts/UI/Typewriter.cs
@@ -1,60 +1,36 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using TMPro;
 using NaughtyAttributes;
 
 namespace Enter
 {
+    [RequireComponent(typeof(RectTransform))]
     public class Typewriter : MonoBehaviour {
-        protected TMP_Text textMesh;
-        protected Mesh mesh;
-        protected Vector3[] originalVertex;
-        protected Vector3[] vertices;
-        protected RectTransform rectTransform;
+        RectTransform rectTransform;
+        RectTransform parentRecTransform;
+
+        TMP_Text textMesh;
+        string _finishedText;
 
         public void Awake() {
             textMesh = GetComponentInChildren<TMP_Text>();
-            rectTransform = GetComponentInChildren<RectTransform>();
+            rectTransform = GetComponent<RectTransform>();
+            parentRecTransform = transform.parent?.GetComponent<RectTransform>();
+            _finishedText = textMesh.text;
         }
 
-        void Start() {
-            textMesh.ForceMeshUpdate();
-            mesh = textMesh.mesh;
-            originalVertex = mesh.vertices;
-            vertices = mesh.vertices;
-        }
-
-        [Button("Erase")]
-        public void EraseAllText() {
-            for (int i = 0; i < textMesh.textInfo.characterCount; i++) {
-                TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
-                if (c.character == ' ') continue;
-                int index = c.vertexIndex;
-                for (int j = 0; j < 4; j++)
-                    vertices[index+j] = Vector2.zero;
-            }
-            mesh.vertices = vertices;
-            textMesh.canvasRenderer.SetMesh(mesh);
+        void OnEnable() {
+            textMesh.text = "";
         }
 
         public IEnumerator WaitForTypeWrite(float _charactersPerMinute) {
-            for (int i = 0; i < textMesh.textInfo.characterCount; i++)
-            {
-                TMP_CharacterInfo c = textMesh.textInfo.characterInfo[i];
-                if (c.character == ' ') continue;
-                else {
-                    /* textMesh.ForceMeshUpdate(); */
-                    mesh = textMesh.mesh;
-                    vertices = mesh.vertices;
-
-                    int index = c.vertexIndex;
-                    for (int j = 0; j < 4; j++) 
-                        vertices[index+j] = originalVertex[index+j];
-
-                    mesh.vertices = vertices;
-                    textMesh.canvasRenderer.SetMesh(mesh);
-                    yield return new WaitForSecondsRealtime(60.0f /_charactersPerMinute);
-                }
+            for (int i = 0; i < _finishedText.Length; i++) {
+                textMesh.text = _finishedText.Substring(0, i) + "_";
+                LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
+                if (parentRecTransform) LayoutRebuilder.ForceRebuildLayoutImmediate(parentRecTransform);
+                yield return new WaitForSecondsRealtime(60.0f /_charactersPerMinute);
             }
         }
     }
