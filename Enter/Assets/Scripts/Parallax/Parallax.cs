ce0cd646706c8c325a2a7fca93e284ac627944f9
Zack Cheng <zack_cheng@brown.edu>
2023-05-06 14:51:52 -0400

    wip3

--- Enter/Assets/Scripts/Parallax/Parallax.cs
+++ Enter/Assets/Scripts/Parallax/Parallax.cs
@@ -9,13 +9,13 @@ namespace Enter
   {
     public static Parallax Instance;
     
-    [SerializeField] private Canvas _canvas1;
-    [SerializeField] private Image  _image1;
-    private Material _material1;
-
-    [SerializeField] private Canvas _canvas2;
-    [SerializeField] private Image  _image2;
-    private Material _material2;
+    [SerializeField] private Image _bgImageBackTop;
+    // [SerializeField] private Image _bgImageMidTop;
+    [SerializeField] private Image _bgImageFrontTop;
+    
+    [SerializeField] private Image _bgImageBack;
+    // [SerializeField] private Image _bgImageMid;
+    [SerializeField] private Image _bgImageFront;
 
     [SerializeField] private float _translateCorrectionValue; // an extra to apply after layer translation to make the two backgrounds seamless
     [SerializeField] private int _repeatFactor;
@@ -40,140 +40,37 @@ namespace Enter
       Instance = this;
       transform.SetParent(null);
       DontDestroyOnLoad(gameObject);
-
-      _material1 = new Material(_image1.material);
-      _image1.material = _material1;
-      _material2 = new Material(_image2.material);
-      _image2.material = _material2;
-    }
-
-    void Start()
-    {
-        _startPositions = new List<Vector3>();
-        _imageSizes = new List<Vector2>();
-        _layers = new List<ParallaxItem>(GetComponentsInChildren<ParallaxItem>());
-
-        foreach (ParallaxItem item in _layers) {
-            InitializeLayerInfo(item);
-        }
-
-        RepeatLayers();
     }
 
     void LateUpdate()
     {
-      _image1.material.mainTextureOffset = new Vector2(Time.time, 0);
-      Debug.Log(_image1.material.mainTextureOffset);
-    
-      // TODO: On scene reset, make sure parallax backgrounds stay consistent.
-
-      // now we need to call on lateupdate because 
-      // time is paused during scene transitions, so there won't be a fixed update call
-      UpdateLayers();
-    }
-
-    void FixedUpdate()
-    {
-      // not calling on fixed update would mean jittery movement
-      UpdateLayers();
-    }
-    
-    void UpdateLayers()
-    {
-        for (int i = 0; i < _layers.Count; i++) 
-        {
-            float layerOffset = ComputeCameraOffset(i);
-
-            TranslateLayerPositionIfOffscreen(i, layerOffset);
-
-            MoveLayer(i);
-          }
+      updateLayersHorizontal();
+      updateLayersVertical();
     }
 
     #endregion
 
-
-
     #region ================== Helpers
 
-    private void InitializeLayerInfo(ParallaxItem item)
+    private void updateLayersHorizontal()
     {
-        _startPositions.Add(item.transform.position);
-        _imageSizes.Add(item.GetComponentInChildren<SpriteRenderer>().bounds.size);
+      Vector2 backOffset  = new Vector2(Time.time/4, 0);
+      Vector2 midOffset   = new Vector2(Time.time/2, 0);
+      Vector2 frontOffset = new Vector2(Time.time,   0);
+
+      _bgImageBackTop.material.mainTextureOffset  = backOffset;
+      // _bgImageMidTop.material.mainTextureOffset   = midOffset;
+      _bgImageFrontTop.material.mainTextureOffset = frontOffset;
+
+      _bgImageBack.material.mainTextureOffset  = backOffset;
+      // _bgImageMid.material.mainTextureOffset   = midOffset;
+      _bgImageFront.material.mainTextureOffset = frontOffset;
     }
 
-    private void RepeatLayers()
+    private void updateLayersVertical()
     {
-        List<ParallaxItem> repeatedLayers = new List<ParallaxItem>();
-        for (int i = 0; i < _repeatFactor-1; i++) 
-        {
-            for (int j = 0; j < _layers.Count; j++)
-            {
-                ParallaxItem item = _layers[j];
-                GameObject clonedLayer = Instantiate(
-                    item.gameObject,
-                    item.transform.position + Vector3.right*(i+1)*_imageSizes[j].x,
-                    item.transform.rotation, 
-                    transform
-                );
-                repeatedLayers.Add(clonedLayer.GetComponent<ParallaxItem>());
-                _startPositions.Add(clonedLayer.transform.position);
-            }
-            _imageSizes.AddRange(_imageSizes);
-        }
-
-        // Translate all layers backwards so that the camera is in the center of the repeated layers
-        _layers.AddRange(repeatedLayers);
-        for (int i = 0; i < _layers.Count; i++) 
-        {
-            float translationFactor = 0.5f*_imageSizes[i].x*(_repeatFactor-1);
-            _layers[i].transform.position -= Vector3.right*translationFactor;
-            _startPositions[i] = _layers[i].transform.position; 
-        }
+      // todo
     }
-
-    private float ComputeCameraOffset(int layerIndex)
-    {
-        float parallaxValue = _layers[layerIndex].parallaxValue;
-        Vector2 imageLength = _imageSizes[layerIndex];
-
-        Vector3 relativePos = _camera.transform.position * parallaxValue;   
-        Vector3 dist = _camera.transform.position - relativePos;
-
-        float cameraOffset = _camera.transform.position.x * (1-parallaxValue);
-        return cameraOffset;
-    }
-
-    private void TranslateLayerPositionIfOffscreen(int layerIndex, float cameraOffset)
-    {
-        Vector2 imageLength = _imageSizes[layerIndex];
-        float translationFactor = 0.5f*_imageSizes[layerIndex].x*(_repeatFactor-1);
-        if (cameraOffset + 0.5f*translationFactor > _startPositions[layerIndex].x + imageLength.x)
-        {
-            _startPositions[layerIndex] += new Vector3(imageLength.x*_repeatFactor + _translateCorrectionValue, 0, 0);
-        }
-        if (cameraOffset + 0.5f*translationFactor < _startPositions[layerIndex].x - imageLength.x)
-        {
-            _startPositions[layerIndex] -= new Vector3(imageLength.x*_repeatFactor + _translateCorrectionValue, 0, 0);
-        }
-    }
-
-    private void MoveLayer(int layerIndex)
-    {
-        // Vector3 startPos = _startPositions[layerIndex];
-        // float parallaxValue = _layers[layerIndex].parallaxValue;
-        // float parallaxValueX = _layers[layerIndex].parallaxValueX;
-        // float parallaxValueY = _layers[layerIndex].parallaxValueY;
-
-        // Vector3 relativePos = _camera.transform.position * parallaxValue;
-        // relativePos = Vector2.up * _camera.transform.position.y * parallaxValueY + Vector2.right * _camera.transform.position.x * parallaxValueX;
-        // relativePos.z = startPos.z;
-
-        // _layers[layerIndex].transform.position = startPos + relativePos;
-
-        _layers[layerIndex].transform.position = new Vector3(_camera.transform.position.x, _camera.transform.position.y, _layers[layerIndex].transform.position.z);
-    }
-
 
     #endregion
   }
