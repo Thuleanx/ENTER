94fce8eea76cb34664e1207806554210db00ed0f
DustInMyGit <dustin.h.wu@gmail.com>
2023-05-11 15:08:03 -0400

    Resolved issues with vertical parallax background
    Added initial offset, and x1/x2/x3 are now constants that are set once

--- Enter/Assets/Scripts/Parallax/Parallax.cs
+++ Enter/Assets/Scripts/Parallax/Parallax.cs
@@ -17,14 +17,40 @@ namespace Enter
     [SerializeField] private Image _bgImageMid;
     [SerializeField] private Image _bgImageFront;
 
-    [SerializeField] private float _translateCorrectionValue; // an extra to apply after layer translation to make the two backgrounds seamless
-    [SerializeField] private int _repeatFactor;
-
     private Camera _camera => Camera.main;
-    private List<ParallaxItem> _layers; // background images
 
-    private List<Vector3> _startPositions;
-    private List<Vector2> _imageSizes;
+    // Getters for canvas image positions
+    private RectTransform frontTop => _bgImageFrontTop.GetComponent<RectTransform>();
+    private RectTransform midTop   => _bgImageMidTop.GetComponent<RectTransform>();
+    private RectTransform backTop  => _bgImageBackTop.GetComponent<RectTransform>();
+    private RectTransform front    => _bgImageFront.GetComponent<RectTransform>();
+    private RectTransform mid      => _bgImageMid.GetComponent<RectTransform>();
+    private RectTransform back     => _bgImageBack.GetComponent<RectTransform>();
+
+    // Note: Initial offset for the current backgrounds was measured from the middle position...
+    private float initialFrontTopOffset;
+    private float initialMidTopOffset;
+    private float initialBackTopOffset;
+    private float initialFrontOffset;
+    private float initialMidOffset;
+    private float initialBackOffset;
+
+    // ... Which is why an initial offset of half the max height is needed
+    private float initialCameraOffset;
+    
+    private float x1;
+    private float x2;
+    private float x3;
+    private float h = 720;
+
+    private float aMax = 114; // World space units of highest camera altitude, found via playtesting and might change later
+    private float aMin = 0;  // World space units of lowest camera altitude
+    
+    [field:SerializeField] private float a => _camera.transform.position.y - initialCameraOffset;
+
+    [field:SerializeField] private float x3Offset => -(a / aMax) * (x3 - h);
+    [field:SerializeField] private float x2Offset => -(a / aMax) * (x2 - h);
+    [field:SerializeField] private float x1Offset => -(a / aMax) * (x1 - h);
 
 
     #region ================== Methods
@@ -50,6 +76,10 @@ namespace Enter
       initialFrontOffset    = front.anchoredPosition.y;
       initialMidOffset      = mid.anchoredPosition.y;
       initialBackOffset     = back.anchoredPosition.y;
+      x1 = initialFrontTopOffset - initialFrontOffset;
+      x2 = initialMidTopOffset - initialMidOffset;
+      x3 = initialBackTopOffset - initialBackOffset;
+      initialCameraOffset = aMax / 2; // set the initial offset of the backgrounds to half the max height
     }
 
     void LateUpdate()
@@ -78,64 +108,14 @@ namespace Enter
       _bgImageFront.material.mainTextureOffset = frontOffset;
     }
 
-    private RectTransform frontTop => _bgImageFrontTop.GetComponent<RectTransform>();
-    private RectTransform midTop   => _bgImageMidTop.GetComponent<RectTransform>();
-    private RectTransform backTop  => _bgImageBackTop.GetComponent<RectTransform>();
-    private RectTransform front    => _bgImageFront.GetComponent<RectTransform>();
-    private RectTransform mid      => _bgImageMid.GetComponent<RectTransform>();
-    private RectTransform back     => _bgImageBack.GetComponent<RectTransform>();
-
-    private float initialFrontTopOffset;
-    private float initialMidTopOffset;
-    private float initialBackTopOffset;
-    private float initialFrontOffset;
-    private float initialMidOffset;
-    private float initialBackOffset;
-    
-    private float x1 => frontTop.anchoredPosition.y - front.anchoredPosition.y;
-    private float x2 => midTop.anchoredPosition.y - mid.anchoredPosition.y;
-    private float x3 => backTop.anchoredPosition.y - back.anchoredPosition.y;
-    private float h = 720;
-
-    private float aMax = 29; // World space units of highest camera altitude
-    private float aMin = 0;  // World space units of lowest camera altitude
-    
-    [field:SerializeField] private float a => _camera.transform.position.y;
-
-    [field:SerializeField] private float x3Offset => -a / aMax * (x3 - h);
-    [field:SerializeField] private float x2Offset => -a / aMax * (x2 - h);
-    [field:SerializeField] private float x1Offset => -a / aMax * (x1 - h);
-
     private void updateLayersVertical()
     {
-      Debug.Log("x3Offset" + x3Offset);
-      Debug.Log("x2Offset" + x2Offset);
-      Debug.Log("x1Offset" + x1Offset);
-
       frontTop.anchoredPosition = new Vector2(frontTop.anchoredPosition.x,  initialFrontTopOffset + x1Offset);
       midTop.anchoredPosition   = new Vector2(midTop.anchoredPosition.x,    initialMidTopOffset   + x2Offset);
       backTop.anchoredPosition  = new Vector2(backTop.anchoredPosition.x,   initialBackTopOffset  + x3Offset);
       front.anchoredPosition    = new Vector2(front.anchoredPosition.x,     initialFrontOffset    + x1Offset);
       mid.anchoredPosition      = new Vector2(mid.anchoredPosition.x,       initialMidOffset      + x2Offset);
       back.anchoredPosition     = new Vector2(back.anchoredPosition.x,      initialBackOffset     + x3Offset);
-
-      // float cameraY = _camera.transform.position.y;
-      // Vector2 backOffset  = new Vector2(0, cameraY / 3000);
-      // Vector2 midOffset   = new Vector2(0, cameraY / 1500);
-      // Vector2 frontOffset = new Vector2(0,  cameraY / 750);
-
-      // _bgImageBackTop.material.mainTextureOffset  += backOffset;
-      // _bgImageMidTop.material.mainTextureOffset   += midOffset;
-      // _bgImageFrontTop.material.mainTextureOffset += frontOffset;
-
-      // _bgImageBack.material.mainTextureOffset  += backOffset;
-      // _bgImageMid.material.mainTextureOffset   += midOffset;
-      // _bgImageFront.material.mainTextureOffset += frontOffset;
-
-      // float x = _bgImageFrontTop.transform.position.x;
-      // float y = _bgImageFrontTop.transform.position.y;
-      // float z = _bgImageFrontTop.transform.position.z;
-      // _bgImageFrontTop.transform.position = new Vector3(x, cameraY / 1000, z);
     }
 
     #endregion
